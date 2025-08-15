bbf265050a99eef04efae670ca896050d021d5dd
pxchecao <pepe1332016@gmail.com>
2025-08-15 10:22:12 -0300

    Add files via upload

--- market pacheco/Login/script.js
+++ market pacheco/Login/script.js
@@ -2,7 +2,7 @@ function login(){
     var nome = $('#nome').val()
     var senha = $('#senha').val()
 
-    if(nome && senha && nome === "admin" && senha === "12345"){
+    if(nome && senha && nome === "mog" && senha === "matheus"){
         const user = {
             name: nome,
             dataEntrada: new Date(),
